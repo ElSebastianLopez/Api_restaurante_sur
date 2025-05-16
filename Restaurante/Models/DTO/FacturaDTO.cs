2511a38b83f88762dcba96f2e476076fa6fd8c93
ElSebastianLopez <sebaslopezhenap@gmail.com>
2025-05-15 22:44:02 -0500

    Commit con servicios iniciales

---
+++ Restaurante/Models/DTO/FacturaDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Restaurante.Models.DTO
+{
+    public class FacturaDTO
+    {
+        public int NroFactura { get; set; }
+        public int IdCliente { get; set; }
+        public int NroMesa { get; set; }
+        public int IdMesero { get; set; }
+        public DateOnly Fecha { get; set; }
+
+        public string? NombreMesero { get; set; }
+        public string? NombreMesa { get; set; }
+
+        public string? NombreCliente { get; set; }
+
+    }
+}
